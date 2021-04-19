ff0dce2dfc14659e326b03e39609c7f715a7e83e
adiffpirate <lpsmonteiro@gmail.com>
2021-04-19 20:30:23 -0300

    Added reflected XSS via form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { DomSanitizer, SafeHtml } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-angular';
+  checkoutForm = this.formBuilder.group({
+    user_input: ''
+  });
+
+  private form_result: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private sanitizer: DomSanitizer,
+  ) {}
+
+  onSubmit(): void {
+    // Process checkout data here
+    console.warn('Input send', this.checkoutForm.value);
+    this.form_result = String(Object.values(this.checkoutForm.value)[0]);
+    this.checkoutForm.reset();
+  }
+
+  getUnsafeResult(): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(this.form_result);
+  }
+
+  getSafeResult(): string {
+    return this.form_result;
+  }
 }
