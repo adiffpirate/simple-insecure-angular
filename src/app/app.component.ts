137265f6393dc438630f825cfc027df2430e79ec
adiffpirate <lpsmonteiro@gmail.com>
2021-04-19 21:21:06 -0300

    Added buttons to differentiate safe and unsafe inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,12 @@ export class AppComponent {
 
   private form_result: string;
 
+  private unsafe_input: SafeHtml;
+  private unsafe_flag: boolean;
+
+  private safe_input: string;
+  private safe_flag: boolean;
+
   constructor(
     private formBuilder: FormBuilder,
     private sanitizer: DomSanitizer,
@@ -25,13 +31,26 @@ export class AppComponent {
     console.warn('Input send', this.checkoutForm.value);
     this.form_result = String(Object.values(this.checkoutForm.value)[0]);
     this.checkoutForm.reset();
+
+    this.safe_input = this.form_result;
+    this.unsafe_input = this.sanitizer.bypassSecurityTrustHtml(this.form_result)
   }
 
-  getUnsafeResult(): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(this.form_result);
+  setUnsafeFlag(): void {
+    this.unsafe_flag = true;
+    this.safe_flag = false;
   }
 
-  getSafeResult(): string {
-    return this.form_result;
+  setSafeFlag(): void {
+    this.unsafe_flag = false;
+    this.safe_flag = true;
+  }
+
+  getResult(): SafeHtml | string {
+    if (this.safe_flag){
+      return this.safe_input;
+    } else {
+      return this.unsafe_input;
+    }
   }
 }
